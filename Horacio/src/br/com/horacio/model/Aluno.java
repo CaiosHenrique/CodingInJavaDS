d18e4d68052160334d894274057fd932b2297bb4
caio <caiokaikai@gmail.com>
2024-02-15 22:13:44 -0300

    Initial Commit!

---
+++ Horacio/src/br/com/horacio/model/Aluno.java
@@ -0,0 +1,16 @@
+package br.com.horacio.model;
+
+public class Aluno extends PessoaModel {
+	
+ private String CursoAlocado;
+
+public String getCursoAlocado() {
+	return CursoAlocado;
+}
+
+public void setCursoAlocado(String cursoAlocado) {
+	CursoAlocado = cursoAlocado;
+}
+
+
+}
